cc45acec4779f61412e0217bec7b4d0324a8b13f
cpscott1 <thacam18@gmail.com>
2019-05-30 02:14:21 -0500

    updated migrations

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ const knexConfig = {
   useNullAsDefault: true,
 }
 
+const db = knex(knexConfig);
+
 server.use(express.json());
 server.use(helmet());
+
+
+server.get('/api/cohorts', (req, res) => {
+  db('cohorts')
+  .then(cohorts => {
+    res.status(200).json(cohorts);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
 
 const port = 5000;
 server.listen(port, () => {
--- migrations/20190529021118_create_cohorts_table.js
+++ migrations/20190529021118_create_cohorts_table.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema
+  return knex.schema.createTable('cohorts', tbl => {
+    tbl.increments();
+
+    tbl
+    .string('name', 128)
+    .notNullable()
+    .unique();
+
+    tbl.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema
+  return knex.schema.dropTableIfExists('cohorts');
 };
